970a52068f2a940b70ca744ea64e5b0944c181f0
durwa004 <durwa004@umn.edu>
2019-10-13 06:44:49 -0500

    Run bcftools for each individual horse

--- bcftools_stats/Generate_bcftools_by_individual.py
+++ bcftools_stats/Generate_bcftools_by_individual.py
@@ -37,35 +37,24 @@ if __name__ == '__main__':
     data = os.path.abspath(args.data)
     horses = os.path.abspath(args.individual) 
 
-    print(f"Using bcftools stats on these files {end}")
-
     header = (
               "#!/bin/bash -l\n"  
               "#PBS -l nodes=1:ppn=4,walltime=06:00:00,mem=2g\n"
               "#PBS -m abe\n"
               "#PBS -M durwa004@umn.edu\n"
-              f"#PBS -o $PBS_JOBID.bcftools_stats{end}.out\n"
-              f"#PBS -e $PBS_JOBID.bcftools_stats{end}.err\n"
-              f"#PBS -N bcftools_stats{end}.pbs\n"
+              f"#PBS -o $PBS_JOBID.bcftools_stats_by_ind.out\n"
+              f"#PBS -e $PBS_JOBID.bcftools_stats_by_ind.err\n"
+              f"#PBS -N bcftools_stats_by_ind.pbs\n"
               "#PBS -q small\n"
               "module load bcftools\n"
              )
     
-    tmp = []
-    tmp_b = []
     with open(horses) as input_file:
         for line in input_file:
             horse,breed = line.rstrip("\n").split("\t")
-            
-            if file_name.endswith(f"{end}"):
-            a = file_name.split(f"{end}")
-            tmp.append(a[0])
-    
-    pbs = os.path.join(os.getcwd(), f"bcftools_stats{end}.pbs")
-    
-    with open(pbs, "w") as f:
-        print(header, file=f)
-        print(f"cd {data}\n", file=f)
-        print("for i in ", " ".join(tmp), "; do "
-             + f"bcftools stats {i}{end}" 
-             + f" > {i}{end}.stats;done", file=f)
+            pbs = os.path.join(os.getcwd(), f"bcftools_stats_{horse}.pbs")
+            #create job script for each individual horse
+            with open(pbs, "w") as f:
+                print(header, file=f)
+                print(f"cd {data}\n", file=f)
+                print(f"bcftools stats -s {horse} thesis_intersect.vcf.gz > {horse}_{breed}.stats", file=f) 
