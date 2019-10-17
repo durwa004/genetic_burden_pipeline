e7787593c013cea301b7d293e95f83a9b9b9cad0
durwa004 <durwa004@umn.edu>
2019-10-17 11:24:18 -0500

    Extract type of variant for GB paper

--- genetic_burden/python_scripts/Get_type_of_variant_annovar.py
+++ genetic_burden/python_scripts/Get_type_of_variant_annovar.py
@@ -1,57 +1,21 @@
-import os
-
-with open("../annovar/annovar_coding.txt", "r") as input_file, open("SnpEff_coding_tidy.txt", "r") as header, open("annovar_variant_type_all.txt", "w") as output_file:
-    info = header.readline()
-    print(info, file = output_file)
+with open("thesis_intersect_annovar.variant_function", "r") as input_file, open("annovar_variant_type_all.txt", "w") as output_file:
+    print("#CHROM\tPOS\tAC\tAF\tConsequence\tImpact\tGene", file = output_file)
     for line in input_file:
         line = line.rstrip("\n").split("\t")
-        consequence = line[1]
-        if "frameshift" in line[1] or "stopgain" in line[1] or "stoplost" in line[1]:
-            impact = "HIGH"
-        elif "nonframeshift" in line[1] or "nonsynonymous" in line[1]:
-            impact = "MODERATE"
-        elif "synonymous SNV" in line[1]:
-            impact = "LOW"
-        elif "unknown" in line[1]:
-            impact = "UNKNOWN"
-        if line[2] == "UNKNOWN":
-            b = line[18].split("AC=")
-            c = b[1].split(";")
-            AC = c[0]
-            if "AF_" in line[18]:
-                e = line[18].split("AF=")
-                f = e[1].split(";")
-                AF = f[0]
+        if "NC_" in line[2]:
+            ab = line[17].split("AC=")
+            bc = ab[1].split(";")
+            AC = bc[0]
+            cd = line[1].split(",")
+            gene = cd[0]
+            if "frameshift" in line[1] or "stopgain" in line[1] or "stoplost" in line[1]:
+                impact = "HIGH"
+            elif "nonframeshift" in line[1] or "nonsynonymous" in line[1]:
+                impact = "MODERATE"
+            elif "synonymous SNV" in line[1]:
+                impact = "LOW"
             else:
-                AF = "NA"
-            print("\t".join(line[11:18]), AC, AF, consequence, impact, "NA", "NA", "NA", "NA", "\t".join(line[20:]), file = output_file, sep = "\t")
+                impact = "UNKNOWN"
+            print(line[2], line[3],AC,line[7],line[0],"NA", gene, file = output_file, sep  = "\t")
         else:
-            a = line[2].split(":")
-            if a[2] == "wholegene,":
-                gene = a[0]
-                coding = "NA"
-                protein = "NA"
-                b = line[18].split("AC=")
-                c = b[1].split(";")
-                AC = c[0]
-                if "AF_" in line[18]:
-                    e = line[18].split("AF=")
-                    f = e[1].split(";")
-                    AF = f[0]
-                else:
-                    AF = "NA"
-                print("\t".join(line[11:18]), AC, AF, consequence, impact, gene, "NA", coding, protein, "\t".join(line[20:]), file = output_file, sep = "\t")
-            else:
-                gene = a[0]
-                coding = a[3]
-                protein = a[4]
-                b = line[18].split("AC=")
-                c = b[1].split(";")
-                AC = c[0]
-                if "AF_" in line[18]:
-                    e = line[18].split("AF=")
-                    f = e[1].split(";")
-                    AF = f[0]
-                else:
-                    AF = "NA"
-                print("\t".join(line[11:18]), AC, AF, consequence, impact, gene, "NA", coding, protein, "\t".join(line[20:]), file = output_file, sep = "\t")
+            next
