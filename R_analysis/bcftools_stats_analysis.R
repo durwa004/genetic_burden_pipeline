35c232e56a1be65db6ff3f1255faa30d196d1909
Sian Durward-Akhurst <durwa004@umn.edu>
2019-10-22 13:19:41 -0500

    Modified for sprint

--- R_analysis/bcftools_stats_analysis.R
+++ R_analysis/bcftools_stats_analysis.R
@@ -1,6 +1,9 @@
 library(ggplot2)
 library(cowplot)
 library(dplyr)
+library(forcats)
+library(devtools)
+library(ggpubr)
 
 #Only include autosomes and chr X (not MT and unplaced contigs)
 setwd("/Users/durwa004/Documents/PhD/Projects/1000_genomes/GB_project/genetic_burden_pipeline/R_analysis/")
@@ -67,16 +70,48 @@ intersect_stats <- read.table("../../bcftools_stats_output/intersect_by_ind_numb
 intersect_stats$nvariants <- intersect_stats$nNonRefHom + intersect_stats$nHets
 mean(intersect_stats$nvariants)
 intersect_stats$tstv <- intersect_stats$Ts/intersect_stats$Tv
+#Add in DOC info
+DOC <- read.table("../../bcftools_stats_output/DOC_by_horse.txt", header=T)
+
+intersect_doc <- merge(intersect_stats,DOC, by="Sample")
+
+#Plot DOC histogram
+x = ggplot(intersect_doc, aes(x=nuclear_placed_DOC)) + theme_bw() + ylab("Frequency") + 
+  xlab("Depth of coverage") + geom_histogram(fill=rgb(122/255,0/255,25/255,1)) +scale_y_continuous(labels=comma) + 
+  theme(panel.grid = element_blank(), panel.border = element_blank(), axis.line.x = element_line(),
+        axis.line.y = element_line(), axis.text.x = element_text(angle=90), axis.text = element_text(size=10), axis.title = element_text(size=12,face="bold"))
+save_plot("548_individuals_DOC.tiff", x, base_height = 3.5, base_width = 6)
+
+#Look for correlation between number of variants and DOC
+x = ggplot(intersect_doc, aes(x=nuclear_placed_DOC,y=nvariants)) + theme_bw() + ylab("Number of variants") + 
+  xlab("Depth of coverage") + geom_point(fill=rgb(122/255,0/255,25/255,1)) +scale_y_continuous(labels=comma) + 
+  theme(panel.grid = element_blank(), panel.border = element_blank(), axis.line.x = element_line(),
+        axis.line.y = element_line(), axis.text.x = element_text(angle=90), axis.text = element_text(size=10), axis.title = element_text(size=12,face="bold"))
+save_plot("548_individuals_DOC_nvariants.tiff", x, base_height = 3.5, base_width = 6)
+cor(intersect_doc$nuclear_placed_DOC,intersect_doc$nvariants, method = "pearson")
+cor.test(intersect_doc$nuclear_placed_DOC,intersect_doc$nvariants, method = "pearson")
 
 ####Figure out differences in the number of variants per breed
-intersect_stats_br <- intersect_stats %>% 
+intersect_stats_br <- intersect_doc %>% 
   filter(!grepl('Other', breed))
+fit1 <- (lm(nvariants ~ breed + nuclear_placed_DOC, data=intersect_stats_br))
+fit2 <- (lm(nvariants ~ breed, data=intersect_stats_br))
+anova(fit1,fit2)
+
 kruskal.test(intersect_stats_br$nvariants, intersect_stats_br$breed)
 x = ggplot(intersect_stats_br, aes(x=breed, y=nvariants)) + theme_bw() + ylab("Number of variants") + 
   xlab("Breed") + geom_boxplot(fill=rgb(122/255,0/255,25/255,1)) +scale_y_continuous(labels=comma) + 
   theme(panel.grid = element_blank(), panel.border = element_blank(), axis.line.x = element_line(),
         axis.line.y = element_line(), axis.text.x = element_text(angle=90), axis.text = element_text(size=10), axis.title = element_text(size=12,face="bold"))
 save_plot("10_breeds_nvariants.tiff", x, base_height = 3.5, base_width = 6)
+
+#DOC
+kruskal.test(intersect_stats_br$nuclear_placed_DOC, intersect_stats_br$breed)
+x = ggplot(intersect_stats_br, aes(x=breed, y=nuclear_placed_DOC)) + theme_bw() + ylab("Depth of coverage") + 
+  xlab("Breed") + geom_boxplot(fill=rgb(122/255,0/255,25/255,1)) +scale_y_continuous(labels=comma) + 
+  theme(panel.grid = element_blank(), panel.border = element_blank(), axis.line.x = element_line(),
+        axis.line.y = element_line(), axis.text.x = element_text(angle=90), axis.text = element_text(size=10), axis.title = element_text(size=12,face="bold"))
+save_plot("10_breeds_DOC.tiff", x, base_height = 3.5, base_width = 6)
 
 #### Number of indels per individual
 mean(intersect_stats$nIndels)
@@ -101,6 +136,7 @@ x = ggplot(intersect_stats_br, aes(x=breed, y=tstv)) + theme_bw() + ylab("Number
 save_plot("10_breeds_tstv.tiff", x, base_height = 3.5, base_width = 6)
 
 
+
 ####AF differences by breed
 setwd("/Users/durwa004/Documents/PhD/Projects/1000_genomes/GB_project/bcftools_stats_output/")
 #Categorize AF: Using python
@@ -112,7 +148,7 @@ AF_data$AF <- af$AF
 
 #AF
 x = ggplot(AF_data, aes(x=AF, y=means)) + theme_bw() + ylab("Number of variants") + 
-  xlab("AF category") + geom_point(fill=rgb(122/255,0/255,25/255,1)) + scale_y_continuous(labels=comma) + 
+  xlab("Allele frequency") + geom_point(fill=rgb(122/255,0/255,25/255,1)) + scale_y_continuous(labels=comma) + 
   theme(panel.grid = element_blank(), panel.border = element_blank(), axis.line.x = element_line(),
         axis.line.y = element_line(), axis.text.x = element_text(angle=90), axis.text = element_text(size=10), axis.title = element_text(size=12,face="bold"))
 save_plot("548_horses_AF.tiff", x, base_height = 3.5, base_width = 6)
@@ -120,7 +156,8 @@ save_plot("548_horses_AF.tiff", x, base_height = 3.5, base_width = 6)
 #AF categories
 categories <- read.table("all_horses_AF_categorized.txt", header= T)
 categories$means <- AF_data$means
-x = ggplot(categories, aes(x=AF, y=means)) + theme_bw() + ylab("Number of variants") + 
+categories$AF <- factor(categories$AF,levels = c("<1%", "1-<5%", "5-<10%", "10-<25%", "25-<50%","50-<75%", "75-<100%"))
+x = ggplot(categories, aes(x = AF, y = means)) + theme_bw() + ylab("Number of variants") + 
   xlab("AF category") + geom_boxplot(fill=rgb(122/255,0/255,25/255,1)) + scale_y_continuous(labels=comma) + 
   theme(panel.grid = element_blank(), panel.border = element_blank(), axis.line.x = element_line(),
         axis.line.y = element_line(), axis.text.x = element_text(angle=90), axis.text = element_text(size=10), axis.title = element_text(size=12,face="bold"))
@@ -139,3 +176,6 @@ x = ggplot(singleton_data, aes(x=V2, y=V3)) + theme_bw() + ylab("Number of singl
   theme(panel.grid = element_blank(), panel.border = element_blank(), axis.line.x = element_line(),
         axis.line.y = element_line(), axis.text.x = element_text(angle=90), axis.text = element_text(size=10), axis.title = element_text(size=12,face="bold"))
 save_plot("10_breeds_singletons.tiff", x, base_height = 3.5, base_width = 6)
+
+#Add in DOC info
+DOC <- read.table("../../bcftools_stats_output/DOC_by_horse.txt", header=T)
