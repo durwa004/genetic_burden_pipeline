f1ed844f2f55be0b4697a182c3258f0b0dfa3eaf
durwa004 <durwa004@umn.edu>
2019-10-22 14:26:12 -0500

    Updated

---
+++ split_by_breed/python_scripts/Extract_breed_pop_shared_variant_information.py
@@ -0,0 +1,81 @@
+#Common breed rare population
+common_breed = {}
+with open("common_breed_rare_pop_shared_variants.txt", "r") as input_file:
+    for line in input_file:
+        line = line.rstrip("\n").split("\t")
+        ab = line[0] + ":" + line[1]
+        common_breed[ab] = line[2] + ":" + line[3] + ":" + line[4] + ":" + line[5]
+ 
+with open("common_breed_rare_pop_snpeff_variants.txt","r") as input_file, open("common_breed_rare_pop_snpeff_info.txt", "w") as output_file:
+    print("chromosome\tposition\tpop_AC\tpop_AF\tconsequence\timpact\tgene\tcoding\tprotein\tnuclear\tbreed_common\tbreed_AC\tMultiallelic", file = output_file)
+    for line in input_file:
+        line = line.rstrip("\n").split("\t")
+        chrom_pos = line[0] +":" + line[1]
+        ab = line[7].split(";")
+        bc = ab[0].split("AC=")
+        if "," in bc[1]:
+            cd = bc[1].split(",")
+            AC = cd[0]
+        else:
+            AC = ab[1]
+        cd = line[7].split("|")
+        consequence = cd[1]
+        impact = cd[2]
+        gene = cd[3]
+        for i in range(len(cd[0:12])):
+            if "c." in cd[i]:
+                coding = cd[i]
+            elif "p." in cd[i]:
+                protein = cd[i]
+            elif "n." in cd[i]:
+                nuclear = cd[i]
+            else:
+                coding = "NA"
+                protein = "NA"
+        if chrom_pos in common_breed.keys():
+            breed_info = common_breed[chrom_pos]
+            breed_info = breed_info.split(":")
+            print(line[0], line[1], AC, breed_info[2], consequence, impact, gene, coding, protein,nuclear,breed_info[0],breed_info[1],breed_info[3], file = output_file, sep = "\t")
+        else:
+            print(chrom_pos)
+
+#Rare breed common population
+rare_breed = {}
+with open("../rare_breed_common_po/rare_breed_common_pop_shared_variants.txt", "r") as input_file:
+    for line in input_file:
+        line = line.rstrip("\n").split("\t")
+        ab = line[0] + ":" + line[1]
+        rare_breed[ab] = line[2] + ":" + line[3] + ":" + line[4] + ":" + line[5]
+
+with open("../rare_breed_common_po/rare_breed_common_pop_snpeff_variants.txt","r") as input_file, open("../rare_breed_common_po/rare_breed_common_pop_snpeff_info.txt", "w") as output_file:
+    print("chromosome\tposition\tpop_AC\tpop_AF\tconsequence\timpact\tgene\tcoding\tprotein\tnuclear\tbreed_rare\tbreed_AC\tMultiallelic", file = output_file)
+    for line in input_file:
+        line = line.rstrip("\n").split("\t")
+        chrom_pos = line[0] +":" + line[1]
+        ab = line[7].split(";")
+        bc = ab[0].split("AC=")
+        if "," in bc[1]:
+            cd = bc[1].split(",")
+            AC = cd[0]
+        else:
+            AC = ab[1]
+        cd = line[7].split("|")
+        consequence = cd[1]
+        impact = cd[2]
+        gene = cd[3]
+        for i in range(len(cd[0:12])):
+            if "c." in cd[i]:
+                coding = cd[i]
+            elif "p." in cd[i]:
+                protein = cd[i]
+            elif "n." in cd[i]:
+                nuclear = cd[i]
+            else:
+                coding = "NA"
+                protein = "NA"
+        if chrom_pos in rare_breed.keys():
+            breed_info = rare_breed[chrom_pos]
+            breed_info = breed_info.split(":")
+            print(line[0], line[1], AC, breed_info[2], consequence, impact, gene, coding, protein,nuclear,breed_info[0],breed_info[1],breed_info[3], file = output_file, sep = "\t")
+        else:
+            print(chrom_pos)
---
+++ split_by_breed/python_scripts/Generate_extract_variants.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 11:04:17 2019
+@author: jonahcullen
+"""
+
+import argparse
+import os
+import gzip
+
+def make_arg_parser():
+    parser = argparse.ArgumentParser(
+            prog="GeneratePBS.py",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+            "-d", "--data",
+            default=argparse.SUPPRESS,
+            metavar="",
+            required=True,
+            help="Path to dir for output files  [required]")
+    parser.add_argument(
+            "-v", "--vcf",
+            default=argparse.SUPPRESS,
+            metavar="",
+            required=True,
+            help="vcf to extract variants from [required]")
+    parser.add_argument(
+            "-l", "--list_v",
+            default=argparse.SUPPRESS,
+            metavar="",
+            required=True,
+            help="List of variants (no header) chrom \t pos format [required]")
+    return parser
+
+
+if __name__ == '__main__':
+     
+    parser = make_arg_parser()
+    args = parser.parse_args()
+
+    data = os.path.abspath(args.data)
+    input_file = args.vcf
+    variants = args.list_v
+
+    header = (
+              "#!/bin/bash -l\n"  
+              "#PBS -l nodes=1:ppn=2,walltime=12:00:00,mem=4g\n"
+              "#PBS -m abe\n"
+              "#PBS -M durwa004@umn.edu\n"
+              "#PBS -o $PBS_JOBID.Extract_variants.out\n"
+              "#PBS -e $PBS_JOBID.Extract_variants.err\n"
+              "#PBS -N Extract_variants.pbs\n"
+              "#PBS -q small\n"
+             )
+
+    chrom_pos = []
+    with open(variants) as v_list:
+        for line in v_list:
+            line = line.rstrip("\n").split("\t")
+            ab = line[0] + ":" + line[1] + "-" + line[1]
+            chrom_pos.append(ab)
+
+    pbs = os.path.join(os.getcwd(),"Extract_variants.pbs")
+    
+    with open(pbs, "w") as f:
+        print(header, file=f)
+        print(f"cd {data}\n", file=f)
+        for i in range(len(chrom_pos)):
+            print(f"/home/mccuem/shared/.local/bin/tabix {input_file} ", chrom_pos[i], " > ", chrom_pos[i], ".txt", file = f, sep = "")
