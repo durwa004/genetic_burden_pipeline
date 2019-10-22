ed6ad3781eb100e8da0801669e5d0e0a1df183dc
durwa004 <durwa004@umn.edu>
2019-10-22 14:34:01 -0500

    Need to figure out how to get the coding/protein information from the snpeff file

--- split_by_breed/python_scripts/Extract_breed_pop_shared_variant_information.py
+++ split_by_breed/python_scripts/Extract_breed_pop_shared_variant_information.py
@@ -41,14 +41,14 @@ with open("common_breed_rare_pop_snpeff_variants.txt","r") as input_file, open("
 
 #Rare breed common population
 rare_breed = {}
-with open("../rare_breed_common_po/rare_breed_common_pop_shared_variants.txt", "r") as input_file:
+with open("../rare_breed_common_pop/rare_breed_common_pop_shared_variants.txt", "r") as input_file:
     for line in input_file:
         line = line.rstrip("\n").split("\t")
         ab = line[0] + ":" + line[1]
-        rare_breed[ab] = line[2] + ":" + line[3] + ":" + line[4] + ":" + line[5]
+        rare_breed[ab] = ":".join(line[2:])
 
-with open("../rare_breed_common_po/rare_breed_common_pop_snpeff_variants.txt","r") as input_file, open("../rare_breed_common_po/rare_breed_common_pop_snpeff_info.txt", "w") as output_file:
-    print("chromosome\tposition\tpop_AC\tpop_AF\tconsequence\timpact\tgene\tcoding\tprotein\tnuclear\tbreed_rare\tbreed_AC\tMultiallelic", file = output_file)
+with open("../rare_breed_common_pop/rare_breed_common_pop_snpeff_variants.txt","r") as input_file, open("../rare_breed_common_pop/rare_breed_common_pop_snpeff_info.txt", "w") as output_file:
+    print("chromosome\tposition\tpop_AC\tpop_AF\tconsequence\timpact\tgene\tcoding\tprotein\tnuclear\tbreed_rare\tbreed_AC\tMultiallelic\tother_breed_details", file = output_file)
     for line in input_file:
         line = line.rstrip("\n").split("\t")
         chrom_pos = line[0] +":" + line[1]
@@ -76,6 +76,6 @@ with open("../rare_breed_common_po/rare_breed_common_pop_snpeff_variants.txt","r
         if chrom_pos in rare_breed.keys():
             breed_info = rare_breed[chrom_pos]
             breed_info = breed_info.split(":")
-            print(line[0], line[1], AC, breed_info[2], consequence, impact, gene, coding, protein,nuclear,breed_info[0],breed_info[1],breed_info[3], file = output_file, sep = "\t")
+            print(line[0], line[1], AC, breed_info[2], consequence, impact, gene, coding, protein,nuclear,breed_info[0],breed_info[1],"\t".join(breed_info[3:]), file = output_file, sep = "\t")
         else:
             print(chrom_pos)
