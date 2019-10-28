5becccf383651a66da38617bd0fcfaee1e699052
durwa004 <durwa004@umn.edu>
2019-10-28 17:26:05 -0500

    Remove Prze horses from dataset

--- genetic_burden/python_scripts/convert_breeds.py
+++ genetic_burden/python_scripts/convert_breeds.py
@@ -1,7 +1,7 @@
 #Convert ibio breeds (from Samples spreadsheet) to breeds for GB analysis
 
 with open("horse_genomes_breeds.txt", "r") as input_file, open("horse_genomes_breeds_tidy.txt","w") as output_file:
-    print("horse_id", "breed",  file = output_file, sep = "\t")i
+    print("horse_id", "breed",  file = output_file, sep = "\t")
     input_file.readline()
     for line in input_file:
         horse,breed = line.rstrip("\n").split("\t")
@@ -25,5 +25,15 @@ with open("horse_genomes_breeds.txt", "r") as input_file, open("horse_genomes_br
             print(horse, "TB", file = output_file, sep = "\t")
         elif breed == "Welsh Pony":
             print(horse, "WP", file = output_file, sep = "\t")
+        elif breed == "Przewalski":
+            next
         else:
             print(horse, "Other", file = output_file, sep = "\t")
+
+#Print out list of ids for bcftools view
+with open("horse_genomes_breeds_tidy.txt","r") as input_file, open("joint_gvcf/joint_intersect/horse_ids_all.txt", "w") as output_file:
+    input_file.readline()
+    for line in input_file:
+        horse,breed = line.rstrip("\n").split("\t")
+        print(horse, file = output_file)
+
