ab1181ef63622ec210d87acdc7cb5dc8987b66b3
durwa004 <durwa004@umn.edu>
2019-11-07 15:42:14 -0600

    Create pbs script for bcftools by disease

---
+++ bcftools_stats/Generate_bcftools_by_disease.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  5 11:04:17 2019
+
+@author: jonahcullen
+"""
+
+import argparse
+import os
+
+
+def make_arg_parser():
+    parser = argparse.ArgumentParser(
+            prog="GeneratePBS.py",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+            "-d", "--data",
+            default=argparse.SUPPRESS,
+            metavar="",
+            required=True,
+            help="Path to dir containing the ibio output files of interest [required]")
+    parser.add_argument(
+            "-dz", "--disease",
+            default=argparse.SUPPRESS,
+            metavar="",
+            required=True,
+            help="Disease that you want to get bcftools stats for [required]")
+    parser.add_argument(
+            "-id", "--idlist",
+            default=argparse.SUPPRESS,
+            metavar="",
+            required=True,
+            help="Text file including disease and horse id [required]")
+    return parser
+
+
+if __name__ == '__main__':
+     
+    parser = make_arg_parser()
+    args = parser.parse_args()
+
+    data = os.path.abspath(args.data)
+    dz = args.disease 
+    ids = os.path.abspath(args.idlist)
+
+    header = (
+              "#!/bin/bash -l\n"  
+              "#PBS -l nodes=1:ppn=4,walltime=06:00:00,mem=2g\n"
+              "#PBS -m abe\n"
+              "#PBS -M durwa004@umn.edu\n"
+              f"#PBS -o $PBS_JOBID.bcftools_stats_by_ind.out\n"
+              f"#PBS -e $PBS_JOBID.bcftools_stats_by_ind.err\n"
+              f"#PBS -N bcftools_stats_by_ind.pbs\n"
+              "#PBS -q small\n"
+              "module load bcftools\n"
+             )
+    
+    with open(ids) as input_file:
+        for line in input_file:
+            horse,breed = line.rstrip("\n").split("\t")
+            pbs = os.path.join(os.getcwd(), f"bcftools_stats_{horse}.pbs")
+            #create job script for each individual horse
+            with open(pbs, "w") as f:
+                print(header, file=f)
+                print(f"cd {data}\n", file=f)
+                print(f"bcftools stats -s {horse} thesis_intersect.vcf.gz > ind_bcftools_stats_files/{horse}_{breed}.stats", file=f) 
