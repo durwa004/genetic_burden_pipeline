78f469bad655e8c7fd842990b5d5d5a7d9d035f0
durwa004 <durwa004@umn.edu>
2019-11-22 06:34:25 -0600

    Updated to get information for paper

--- genetic_burden/python_scripts/Extract_coverage.py
+++ genetic_burden/python_scripts/Extract_coverage.py
@@ -29,32 +29,44 @@ if __name__ == '__main__':
 
     data = os.path.abspath(args.data)
 
-    DOC = f"{data}/DOC_totals/DOC_by_horse.txt"
+    DOC = f"{data}/summary_files/DOC_by_horse.txt"
+    reads = f"{data}/summary_files/reads_by_horse.txt"
 
-    with open(DOC, "w") as DOC_file:
+    with open(DOC, "w") as DOC_file, open(reads, "w") as reads_file:
         count =0
         overall_DOC = 0
         print("HORSE\ttotal_DOC\tnuclear_placed_DOC", file = DOC_file)
-        for file_name in os.listdir(data):
-            count +=1
-            a = file_name.split("_coverage.tsv")
-            horse = a[0]
-            print(f"Processing {horse}")
-            with open(data + "/" + file_name,"r") as f:
-                horse_DOC = 0
-                chrom = 0
-                for line in f:
-                    line = line.rstrip("\n").split("\t")
-                    if line[0] == "Total Average:":
-                        total = line[1]
-                    elif line[0] == "NC_001640.1":
-                        continue
-                    elif "NC_" in line[0]:
-                        chrom +=1
-                        b = float(line[1])
-                        horse_DOC = horse_DOC + b
-                av_DOC = horse_DOC/chrom
-                print(horse,total,av_DOC, file = DOC_file, sep = "\t")
-            overall_DOC = overall_DOC + av_DOC
-            DOC_DOC = overall_DOC/count
-            print(DOC_DOC)
+        for directory in os.listdir(data):
+            for file_name in os.listdir(data + "/" + directory):
+                if file_name == "coverage.tsv":
+                    count +=1
+                    a = directory.split("_")
+                    horse = a[0]
+                    print(f"Processing {horse}")
+                    with open(data + "/" + directory + "/" + file_name,"r") as f:
+                        horse_DOC = 0
+                        chrom = 0
+                        for line in f:
+                            line = line.rstrip("\n").split("\t")
+                            if line[0] == "Total Average:":
+                                total = line[1]
+                            elif line[0] == "NC_001640.1":
+                                continue
+                            elif "NC_" in line[0]:
+                                chrom +=1
+                                b = float(line[1])
+                                horse_DOC = horse_DOC + b
+                        av_DOC = horse_DOC/chrom
+                        print(horse,total,av_DOC, file = DOC_file, sep = "\t")
+                    overall_DOC = overall_DOC + av_DOC
+                    DOC_DOC = overall_DOC/count
+                    print(DOC_DOC)
+                elif file_name.endswith(".alignment_summary_metrics"):
+                    with open(data + "/" + directory + "/" + file_name, "r") as f2:
+                        b = file_name.split(".metrics.alignment_summary_metrics")
+                        horse = b[0]
+                        for line in f2:
+                            line = line.rstrip("\n").split("\t")
+                            if line[0] == "PAIR":
+                                print(horse, line[15], line[16], file = reads_file, sep = "\t")
+                        
