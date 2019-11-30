8af0d1dc101280a2ece5bdd20de4393ddf617c93
durwa004 <durwa004@umn.edu>
2019-11-29 22:25:25 -0600

    Updated to work with individuals and chroms

--- bcftools_stats/python_scripts/Extract_bcftools_by_ind_by_chrom.py
+++ bcftools_stats/python_scripts/Extract_bcftools_by_ind_by_chrom.py
@@ -65,6 +65,7 @@ if __name__ == '__main__':
     for filename in os.listdir(data):
         if filename.endswith(".stats"):
             a = filename.split("_")
+            horse = a[0] + "." + a[1]
             if "unplaced" in a[2]:
                 c = a[2].split(".stats")
                 b = c[0]
@@ -72,23 +73,12 @@ if __name__ == '__main__':
                 b = "NC_" + a[3] + "_1"
             else:
                 b = "NC_" + a[3] + "_3"
-            for horse in chrom_dict.keys():
-             
-                for chromosome in chrom_dict[horse].keys():
-                    if chromosome == b:
-                        with open(f"{data}/{filename}") as input_file:
-                            for line in input_file:
-                                line = line.rstrip("\n").split("\t")
-                                if line[0] == "PSC":
-                                    for chromosome in chrom_dict.keys():
-                                        details = chrom_dict[horse][b].split(":")
-                                        nRefHom = int(details[0]) + int(line[3])
-                                        nNonRef = int(details[1]) + int(line[4])
-                                        nHets = int(details[2]) + int(line[5])
-                                        Ts = int(details[3]) + int(line[6])
-                                        Tv = int(details[4]) + int(line[7])
-                                        nIndels = int(details[5]) + int(line[8])
-                                        chrom_dict[horse][b] = str(nRefHom) + ":" + str(nNonRef) + ":" + str(nHets) + ":" + str(Ts) +":" + str(Tv) + ":" + str(nIndels)
+            with open(f"{data}/{filename}") as input_file:
+                for line in input_file:
+                    line = line.rstrip("\n").split("\t")
+                    if line[0] == "PSC":
+                        details = line[3] + ":" + line[4] + ":" + line[5] + ":" + line[6] + ":" + line[7] + ":" + line[8]
+                        chrom_dict[horse][b] = details 
 
     with open(info, "w") as info_file:
         print("sample.breed\tchromosome\tnRefHom\tnNonRefHom\tnHets\tTs\tTv\tnIndels", file= info_file)
