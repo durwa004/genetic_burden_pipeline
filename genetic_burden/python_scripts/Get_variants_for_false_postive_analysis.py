ceb3d3de84e36d0621e31f0ea50778f617c3fb23
Sian Durward-Akhurst <durwa004@umn.edu>
2020-07-17 09:09:10 -0500

    Updated to get list of variants to Kelsey

--- genetic_burden/python_scripts/Get_variants_for_false_postive_analysis.py
+++ genetic_burden/python_scripts/Get_variants_for_false_postive_analysis.py
@@ -28,19 +28,44 @@ with open("/Users/durwa004/Documents/Postdoc/Projects/LOF_variants/lof_tidy_brie
 intersect = list(set(list(gb.keys())) & set(list(lof.keys())))
 
 gb_only = list(set(list(gb.keys())) - set(intersect))
+lof_only = list(set(list(lof.keys())) - set(intersect))
+#Get randomly selected 5% of LOF and GB only variants.
 
-#Get randomly selected 10% of LOF and GB only variants.
-
-len(gb_only)/10
-len(lof.keys())/10
-gb_r = random.choices(gb_only, k = 83)
-lof_r = random.choices(list(lof.keys()), k=502)
+len(gb_only) * 0.05
+len(intersect) * 0.05
+gb_only = list(set(gb_only))
+gb_r = random.sample(gb_only, 42)
+lof_r = random.sample(intersect, 249)
 gb_r = gb_r + lof_r
 
 with open("genetic_burden_details_brief_tidy.txt", "r") as input_file, open("gb_validation_random_set.txt", "w") as f:
     for line in input_file:
         line = line.rstrip("\n").split("\t")
         for i,v in enumerate(gb_r):
-            a = v.split(":")
-            if line[1] == a[0] and line[2] == a[1]:
-                print("\t".join(line), file = f)
+                a = v.split(":")
+                if line[1] == a[0] and line[2] == a[1]:
+                    print("\t".join(line), file = f)
+        for i1,v1 in enumerate(lof_only):
+            for item in range(len(gb_r)):
+                if v1 == gb_r[item]:
+                    print("\t".join(line), file = f)
+
+#Get overlap between gb_r and the variants that kelsey has been working on.
+kelsey = {}
+with open("kelsey_done.txt", "r") as input_file:
+    input_file.readline()
+    for line in input_file:
+        line = line.rstrip("\n").split("\t")
+        a = line[0] + ":" + line[1]
+        kelsey[a] = "NA"
+
+to_do = list(set(gb_r) - set(list(kelsey.keys())))
+
+with open("genetic_burden_details_brief_tidy.txt", "r") as input_file, open("gb_validation_to_do.txt", "w") as f:
+    for line in input_file:
+        line = line.rstrip("\n").split("\t")
+        for i,v in enumerate(to_do):
+                a = v.split(":")
+                if line[1] == a[0] and line[2] == a[1]:
+                    print("\t".join(line), file = f)
+
